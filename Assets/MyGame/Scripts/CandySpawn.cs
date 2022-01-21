1cded592464087d3c2e77564b95f569c997ddb92
HATZ01 <noah.hatz01@htl-salzburg.ac.at>
2022-01-21 09:59:52 +0100

    Start coding for spawning, change look of start screen

---
+++ Assets/MyGame/Scripts/CandySpawn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandySpawn : MonoBehaviour
+{
+    public GameObject candyGood;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnCandy", 2, 3);
+        
+    }
+
+    Vector2 GetSpawnPoint()
+    {
+        float x = Random.Range(-420, 420);
+        return new Vector2(x, 8);
+    }
+
+    void SpawnCandy()
+    {
+        Instantiate(candyGood, GetSpawnPoint(), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
